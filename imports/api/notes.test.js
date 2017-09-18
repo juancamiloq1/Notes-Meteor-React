9e5f794c6bc401d2c89b2e43391738619d70b777
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-18 15:38:25 -0500

    Seguimos testeando, se agrego nota de prueba. Section 11 clase 121

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -6,6 +6,17 @@ import { Notes } from './notes';
 if( Meteor.isServer ) {
     describe( 'notes', function () {
 
+        beforeEach( function () {
+            Notes.remove({});
+            Notes.insert({
+                _id: 'testNoteId1',
+                title: 'My title',
+                body: 'My Body for note',
+                updatedAt: 0,
+                userId: 'testUserId1'
+            });
+        });
+
         it( 'Deberia insertar nueva nota', function () {
             const userId = 'testid';
             const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });  //Con esta linea se llama el metodo en api/notes.js
@@ -18,5 +29,25 @@ if( Meteor.isServer ) {
                 Meteor.server.method_handlers['notes.insert']();
             }).toThrow();
         });
+
+        it('Deberia eliminar nota', function(){
+            Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1']);
+
+            expect(Notes.findOne({ _id: 'testNoteId1' })).toBeFalsy();
+        });
+
+        it('Deberia no eliminar nota si no esta autenticado', function(){
+            expect( () => {
+                Meteor.server.method_handlers['notes.remove'].apply( {}, ['testNoteId1']);
+            }).toThrow();
+        });
+
+        it('Deberia no eliminar nota si el Id es invalido', function(){
+            expect( () => {
+                Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1'});
+            }).toThrow();
+        });
+
+
     });
 }

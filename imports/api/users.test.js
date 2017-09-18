d384c1e151de608ba989fe41c39514c145eea1ad
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-18 13:53:11 -0500

    Primeras pruebas

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,36 +1,34 @@
-import expect, { createSpy, spyOn, isSpy } from 'expect'
+import { Meteor } from 'meteor/meteor';
+import expect, { createSpy, spyOn, isSpy } from 'expect';
+import { validateNewUser } from './users';
 
-const add = (a, b) => {
-    if( typeof b != 'number' ) {
-        return a + a;
-    }
-    return a + b;
-};
-
-describe('Suma', function(){
-    it('deberia sumar dos numeros', function() {   // nombre y la funcion a testear
-        const res = add(11,9);
-
-        expect(res).toBe(20);
-
-    });
+if ( Meteor.isServer ) {
+    describe('users', function() {
+        it('Deberia permitir email válido', function() {
+            const testUser = {
+                emails: [
+                    {
+                        address: 'Test@example.com'
+                    }
+                ]
+            };
+            const res = validateNewUser(testUser);
     
-    it('deberia doblar un numero', function (){
-        const res = add(44);
-
-        expect(res).toBe(88);
-    
-    });  
-});
-
-
-
-const square = (a) => a * a;
-
-describe('Al Cuadrado', function(){
-    it('deberia elevar al cuadrado un numero', function(){
-        const res = square(4);
-
-        expect(res).toBe(16);
+            expect(res).toBe(true);
+        });
+        
+        it('Deberia rechazar email inválido', function () {
+            const testUser = {
+                emails: [
+                    {
+                        address: 'Testom'
+                    }
+                ]
+            }
+            expect( () => {
+                validateNewUser(testUser);
+            }).toThrow();
+        });
     });
-});
+}
+

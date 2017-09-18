612c2d8428d118cdaa840d0b7a8d41d207e5f4bd
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-18 13:19:02 -0500

    Expect js.

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,3 +1,5 @@
+import expect, { createSpy, spyOn, isSpy } from 'expect'
+
 const add = (a, b) => {
     if( typeof b != 'number' ) {
         return a + a;
@@ -8,17 +10,16 @@ const add = (a, b) => {
 describe('Suma', function(){
     it('deberia sumar dos numeros', function() {   // nombre y la funcion a testear
         const res = add(11,9);
-        if (res != 20 ) { 
-            throw new Error ('Suma no es igual al valor esperado.')
-        }
+
+        expect(res).toBe(20);
+
     });
     
     it('deberia doblar un numero', function (){
         const res = add(44);
+
+        expect(res).toBe(88);
     
-        if( res != 88){
-            throw new Error('el numero no fue doblado');
-        }
     });  
 });
 
@@ -29,8 +30,7 @@ const square = (a) => a * a;
 describe('Al Cuadrado', function(){
     it('deberia elevar al cuadrado un numero', function(){
         const res = square(4);
-        if (res != 16){
-            throw new Error('este no es el cuadrado');
-        }
+
+        expect(res).toBe(16);
     });
 });

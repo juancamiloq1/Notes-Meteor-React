5b65d42c65694c9a545d56b019bbcd29b90e452a
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-19 09:25:58 -0500

    Seguimos en server tests. Section 11 clase 121 - ERROR en test Deberia actualizar nota. no encuentro el problema. el archivo esta igual al tutorial

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,16 +5,17 @@ import { Notes } from './notes';
 
 if( Meteor.isServer ) {
     describe( 'notes', function () {
+        const noteOne = {
+            _id: 'testNoteId1',
+            title: 'My title',
+            body: 'My Body for note',
+            updatedAt: 0,
+            userId: 'testUserId1'
+        };
 
         beforeEach( function () {
             Notes.remove({});
-            Notes.insert({
-                _id: 'testNoteId1',
-                title: 'My title',
-                body: 'My Body for note',
-                updatedAt: 0,
-                userId: 'testUserId1'
-            });
+            Notes.insert(noteOne);
         });
 
         it( 'Deberia insertar nueva nota', function () {
@@ -31,23 +32,50 @@ if( Meteor.isServer ) {
         });
 
         it('Deberia eliminar nota', function(){
-            Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1']);
+            Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, [noteOne._id]);
 
-            expect(Notes.findOne({ _id: 'testNoteId1' })).toBeFalsy();
+            expect(Notes.findOne({ _id: noteOne._id })).toBeFalsy();
         });
 
         it('Deberia no eliminar nota si no esta autenticado', function(){
             expect( () => {
-                Meteor.server.method_handlers['notes.remove'].apply( {}, ['testNoteId1']);
+                Meteor.server.method_handlers['notes.remove'].apply( {}, [noteOne._id]);
             }).toThrow();
         });
 
         it('Deberia no eliminar nota si el Id es invalido', function(){
             expect( () => {
-                Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1'});
+                Meteor.server.method_handlers['notes.remove'].apply({ 
+                    userId: noteOne.userId
+                });
             }).toThrow();
         });
 
+        it('Deberia actualizar nota', function(){
+            const title = 'Este es un titulo actualizado';
+
+            Meteor.server.method_handlers['notes.update'].apply({
+                userId: noteOne.userId
+            }, [
+                noteOne._id, 
+                { title }
+            ]);
+
+            const note = Notes.findOne( noteOne._id );
+            
+            expect(note.updatedAt).toBeGreaterThan(0);
+
+            expect(note).toContain({
+                title, 
+                body: noteOne.body
+            });
+        });
+
+        it('Deberia throw error si existen extra actualizaciones', function(){
+            expect( () => {
+                Meteor.server.method_handlers['notes.update'].apply({userId: noteOne.userId}, [noteOne._id, { title: 'Nuevo Titulo', name: 'JuanCamilo' }]);
+            }).toThrow();
+        });
 
     });
 }
